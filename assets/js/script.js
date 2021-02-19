4f3fb50a07ac16c2e61d0fee54054169de7f7225
Jhonathan <jhonathanpribeiro@gmail.com>
2021-02-19 13:17:04 -0300

    new methods

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,13 +49,38 @@ class UI {
         document.querySelector('#isbn') = '';
     }
 
+    static showAlert(msg, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(msg));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+
+        //remove o alert em 3 segundos
+        setTimeout(()=> document.querySelector('.alert').remove(),3000);
+    }
+
     static deletarLivro(el) {
         if (el.classList.contains('delete')) {
             el.parentElement.parentElement.remove();
         }
     }
+
 }
 
+//Store Class
+class Store {
+    static getLivros(){
+
+    }
+    static addLivro(livro) {
+
+    }
+    static removeLivro(isbn){
+
+    }
+}
 
 document.addEventListener('DOMContentLoaded', UI.exibirLivros);
 
@@ -68,17 +93,17 @@ document.querySelector('#book-form').addEventListener('submit', (e)=>{
     
     //Validação
     if (titulo === '' || autor === '' || isbn === '') {
-        alert('Por favor preencha todos os campos obrigatórios!');
+        UI.showAlert('Por favor preencha todos os campos obrigatórios!', 'danger');
     } else {
         //Instanciando o livro
         const livro = new Livros(titulo, autor, isbn);
         //adicionando um livro na lista        
         UI.addLivroNaLista(livro);
+        //exibe a mensahem de sucesso
+        UI.showAlert('Livro adicionado com sucesso!','success');
         //limpando campos
         UI.clearFields();
     }
-
-
 });
 
 
@@ -88,6 +113,8 @@ document.querySelector('#book-list').addEventListener('click', (e)=>{
     
     UI.deletarLivro(e.target);
 
+      //exibe a mensahem de sucesso
+      UI.showAlert('Livro removido com sucesso!','success');
     console.log(e.target)
 });
 
